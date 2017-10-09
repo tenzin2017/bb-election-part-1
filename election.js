66e4808678b281cf3bd8e2e88f760c4b025d147e
tenzin2017 <tenzinleo@gmail.com>
2017-10-08 23:48:45 -0400

    ajax and .done method implementation, created li elements with inner html as name and votes retreived  from the responsedata,and append at ul element

--- election.js
+++ election.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    data: '',
+    dataType: 'JSON'
+  }).done(function(responseData){
+
+    var ul = document.querySelector('#voting');
+    for(var i=0; i<responseData.candidates.length; i++){
+      var newLi = document.createElement('li');
+      newLi.innerHTML = 'Name: '+responseData.candidates[i].name+ "   |   "+ '  Votes: '+responseData.candidates[i].votes;
+      console.log(newLi);
+      ul.append(newLi);
+    }
+
+
+  });
 
 });
